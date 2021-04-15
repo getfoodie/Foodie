af65c595db6b54679bd1aa9cce4458f2912bbf9a
Kaileyv <kailey.van@stonybrook.edu>
2021-04-15 15:35:46 -0400

    commit changes to Wheel Screen

---
+++ foodie/foodie/WheelViewController.swift
@@ -0,0 +1,43 @@
+//
+//  WheelViewController.swift
+//  foodie
+//
+//  Created by Kailey Van on 4/15/21.
+//
+
+import UIKit
+
+class WheelViewController: UIViewController {
+
+   
+    @IBAction func SpinOnClick(_ sender: UIButton) {
+        print("Click to Spin Button was clicked")
+    }
+    
+    @IBAction func AddOnClick(_ sender: UIButton) {
+        print("Add Options Button was clicked")
+    }
+    
+    @IBAction func RemoveOnClick(_ sender: UIButton) {
+        print("Remove Options Button was clicked")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
