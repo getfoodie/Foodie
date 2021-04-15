5809b4a9058764c6cb0cf0d540bff712991987f6
Jendy <jendyren@gmail.com>
2021-04-15 11:41:52 -0400

    Button Connection Secured + Autolayout

--- foodie/foodie/CravingsViewController.swift
+++ foodie/foodie/CravingsViewController.swift
@@ -10,24 +10,32 @@ import UIKit
 class CravingsViewController: UIViewController {
 
     @IBAction func VeggiesOnClick(_ sender: UIButton) {
+        print("Veggies Button was clicked")
     }
     
     @IBAction func MeatsOnClick(_ sender: UIButton) {
+        print("Meats Button was clicked")
     }
     
     @IBAction func DrinksOnClick(_ sender: UIButton) {
+        print("Drinks Button was clicked")
     }
     
     @IBAction func SoupsOnClick(_ sender: UIButton) {
+        print("Soups Button was clicked")
     }
     @IBAction func FastFoodOnClick(_ sender: UIButton) {
+        print("Fast Food Button was clicked")
     }
     @IBAction func DessertsOnClick(_ sender: UIButton) {
+        print("Desserts Button was clicked")
     }
     
     @IBAction func CarbsOnClick(_ sender: UIButton) {
+        print("Carbs Button was clicked")
     }
     @IBAction func RandomOnClick(_ sender: UIButton) {
+        print("Random Button was clicked")
     }
     
     override func viewDidLoad() {
