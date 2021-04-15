d9cdd3cc4cd5ed9da06cbbbe428463bb8a97aa3a
ChoiYingY <choiying.yau@stonybrook.edu>
2021-04-15 19:53:09 -0400

    add back ResultsViewController

---
+++ foodie/foodie/ResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultsViewController.swift
+//  foodie
+//
+//  Created by Choi Ying Yau on 4/15/21.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
