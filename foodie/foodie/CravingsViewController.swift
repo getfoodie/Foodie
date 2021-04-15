b7d2b35909027e3195a013abc9c7ab56912f3c23
Jendy <jendyren@gmail.com>
2021-04-15 11:33:09 -0400

    Connecting Buttons & CravingsViewController

---
+++ foodie/foodie/CravingsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  CravingsViewController.swift
+//  foodie
+//
+//  Created by Jendy Ren on 4/15/21.
+//
+
+import UIKit
+
+class CravingsViewController: UIViewController {
+
+    @IBAction func VeggiesOnClick(_ sender: UIButton) {
+    }
+    
+    @IBAction func MeatsOnClick(_ sender: UIButton) {
+    }
+    
+    @IBAction func DrinksOnClick(_ sender: UIButton) {
+    }
+    
+    @IBAction func SoupsOnClick(_ sender: UIButton) {
+    }
+    @IBAction func FastFoodOnClick(_ sender: UIButton) {
+    }
+    @IBAction func DessertsOnClick(_ sender: UIButton) {
+    }
+    
+    @IBAction func CarbsOnClick(_ sender: UIButton) {
+    }
+    @IBAction func RandomOnClick(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
