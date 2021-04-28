6c403fd597d075370d1fd2231bfbec1083ca1e5a
ChoiYingY <ycying1226@gmail.com>
2021-04-28 19:30:38 -0400

    display food result(string+img) on ResultViewController

--- foodie/foodie/ResultsViewController.swift
+++ foodie/foodie/ResultsViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var foodResultLabel: UILabel!
+    @IBOutlet weak var foodResultImage: UIImageView!
+    
+    let randNum = Int.random(in: 0...6)
+    var foodChoice = ["Meat", "Veggies", "Carbs", "Drinks", "Soups", "Desserts", "Fast Food"]
+    var foodImage: [UIImage] = [
+        UIImage(named:"meats")!,
+        UIImage(named:"veggie")!,
+        UIImage(named:"carbs")!,
+        UIImage(named:"drink")!,
+        UIImage(named:"soup")!,
+        UIImage(named:"desserts")!,
+        UIImage(named: "fast food")!
+    ]
+    var updateLabel = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if(updateLabel == true){
+            let randomResult = foodChoice[randNum]
+            print("Your result: " + randomResult)
+            self.foodResultLabel!.text = "\(randomResult)"
+            self.foodResultImage.image = foodImage[randNum]
+        }
         // Do any additional setup after loading the view.
     }
     
