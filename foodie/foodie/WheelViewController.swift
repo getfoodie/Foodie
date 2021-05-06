b6094018ed1df316f34b076b7a704af1a714b310
Kaileyv <kailey.van@stonybrook.edu>
2021-05-06 16:37:10 -0400

    commit foodWheel

--- foodie/foodie/WheelViewController.swift
+++ foodie/foodie/WheelViewController.swift
@@ -6,22 +6,71 @@
 //
 
 import UIKit
+import SwiftFortuneWheel
 
 class WheelViewController: UIViewController {
 
+    @IBOutlet weak var centerView: UIView!{
+            didSet {
+                centerView.layer.cornerRadius = centerView.bounds.width / 2
+                centerView.layer.borderColor = CGColor.init(srgbRed: CGFloat(256), green: CGFloat(256), blue: CGFloat(256), alpha: 1)
+                centerView.layer.borderWidth = 7
+            }
+        }
+    
+    
+    
+    @IBOutlet weak var foodWheel: SwiftFortuneWheel!
+    
+    {
+            didSet {
+                foodWheel.configuration = .variousWheelSimpleConfiguration
+                foodWheel.slices = slices
+                foodWheel.pinImage = "whitePinArrow"
+                
+                foodWheel.pinImageViewCollisionEffect = CollisionEffect(force: 8, angle: 20)
+                
+                foodWheel.edgeCollisionDetectionOn = true
+            }
+        }
+        
+        var prizes = ["Veggies", "Meats", "Drinks", "Soups", "Fast Food", "Desserts", "Carbs", "Ice Cream", "Noodles", "Rice"]
+        
+        lazy var slices: [Slice] = {
+            let slices = prizes.map({ Slice.init(contents: [Slice.ContentType.text(text: $0, preferences: .variousWheelSimpleText)]) })
+            return slices
+        }()
+
+        var finishIndex: Int {
+            return Int.random(in: 0..<foodWheel.slices.count)
+        }
+
+        
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+            centerView.layer.cornerRadius = centerView.bounds.width / 2
+        }
+    
+    
     @IBAction func BackOnClick(_ sender: UIButton) {
         performSegue(withIdentifier: "backToHome", sender: UIButton.self)
     }
     
     @IBAction func SpinOnClick(_ sender: UIButton) {
         print("Click to Spin Button was clicked")
+        
+       // foodWheel.startRotationAnimation(finishIndex: finishIndex, continuousRotationTime: 1) { (finished) in
+         //           print(finished)
+        foodWheel.startRotationAnimation(finishIndex: 0, continuousRotationTime: 1) { (finished) in
+                        print(finished)
+        }
     }
     
-    @IBAction func AddOnClick(_ sender: UIButton) {
+        func AddOnClick(_ sender: UIButton) {
         print("Add Options Button was clicked")
     }
     
-    @IBAction func RemoveOnClick(_ sender: UIButton) {
+        func RemoveOnClick(_ sender: UIButton) {
         print("Remove Options Button was clicked")
     }
     
@@ -44,3 +93,4 @@ class WheelViewController: UIViewController {
     */
 
 }
+
